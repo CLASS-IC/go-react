ba83ecd3dab90e5111c14867124236db2267c3aa
CLASS-IC <colbyrodriguez16@gmail.com>
2024-11-21 21:59:40 -0700

    api is kinda working but is without a DB

--- main.go
+++ main.go
@@ -3,10 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello Worlds")
 	app := fiber.New()
-	app.Listen(":4000")
+
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello Worlds"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} //{id:0,completed:False,body:""}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"msg": "Body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.Status(201).JSON(todo)
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
